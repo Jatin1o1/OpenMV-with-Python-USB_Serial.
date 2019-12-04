a8eec75466295bc5430755d9987655588acafe58
Jatin Goyal <40102297+Jatin1o1@users.noreply.github.com>
2019-12-04 17:57:36 +0530

    Update Python-Script.py

--- Python-Script.py
+++ Python-Script.py
@@ -1,13 +1,12 @@
+import serial    #importing serial communication library
+port = 'COM9'       # setting the communication port where my camera is connected
 
-import serial
-port = 'COM9'
 sp = serial.Serial(port, baudrate=115200)
 
 print("starting")
 str=""
 while 1:
     b= sp.read().decode('utf-8')  # decoing the input character from the camera
-    
     if b is not "*":        #  condition to check the string termination for character concatenation.
         str += b
     else:                   # condition is terminating character is found then print the string and then rest the sting
