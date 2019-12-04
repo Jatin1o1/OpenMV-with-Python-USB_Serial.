5a91550b5a1a7b59034bfd7fed227a9259faa604
Jatin Goyal <40102297+Jatin1o1@users.noreply.github.com>
2019-12-04 16:48:53 +0530

    updated main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-import sensor
-from pyb import USB_VCP
-usb = USB_VCP()
-while(True):
-	usb.send("hi123")
+from pyb import USB_VCP   # importing the library for USb VCP(Virtual Comm Port).
+usb = USB_VCP()   # makes USB object
+
+while(True):                # making a loop to continously send data
+    usb.send("hi123")       # send function to send script.
+
+
+
+
+'''  NOTE
+ usb.send() fucntion send the data character by character.
+ for ex: usb.send("hi123") send the "hi123" string as "h", "i", "1", "2", "3".
+'''
+
